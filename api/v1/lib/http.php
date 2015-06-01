22afb652fe5bd86f47e73d9d629d271188c7472c
Clercin Guillaume <gclercin@intellique.com>
2015-06-01 11:38:25 +0200

    Amélioration de la doc interne

--- api/v1/lib/http.php
+++ api/v1/lib/http.php
@@ -1,12 +1,26 @@
 <?php
+	/**
+	 * \brief Constant for HTTP DELETE method
+	 */
 	define('HTTP_DELETE', 1);
+	/**
+	 * \brief Constant for HTTP GET method
+	 */
 	define('HTTP_GET', 2);
+	/**
+	 * \brief Constant for HTTP POST method
+	 */
 	define('HTTP_POST', 4);
+	/**
+	 * \brief Constant for HTTP PUT method
+	 */
 	define('HTTP_PUT', 8);
 
+	/**
+	 * \brief return allowed http methods to the client
+	 * \param $methods[in] : list of http methods
+	 */
 	function httpOptionsMethod($methods) {
-		http_response_code(200);
-
 		$allow = 'OPTIONS';
 		if ($methods & HTTP_DELETE)
 			$allow .= ', DELETE';
@@ -17,9 +31,13 @@
 		if ($methods & HTTP_PUT)
 			$allow .= ', PUT';
 
+		http_response_code(200);
 		header("Allow: " . $allow);
 	}
 
+	/**
+	 * \brief return that http method is not supported
+	 */
 	function httpUnsupportedMethod() {
 		header("Content-Type: application/json; charset=utf-8");
 		http_response_code(405);
