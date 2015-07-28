ea58b7a2fe41214f9d1ede364adb18b1326d7818
Ludovic Noël <lnoel@intellique.com>
2015-07-28 12:55:50 +0200

    2 Scripts de tests sur l'API :
    - Create/Add archive
    - Restore archive

--- examples/restore-archive.py
+++ examples/restore-archive.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python3
-# Archival task creation : authenticate and create an archival task
+# Restoration task creation : authenticate and create a restoration task
 
 import getpass, http.client, json, sys
 from optparse import OptionParser, OptionGroup
@@ -7,21 +7,17 @@ from datetime import datetime
 
 parser = OptionParser()
 
-group = OptionGroup(parser, "create archive options");
-group.add_option("-a", "--archive-name", dest="archiveName", help="Specify archive name (to create an archive)")
-group.add_option("-A", "--archive-id", dest="archiveId", type="int", help="Specify archive id (to adding files to archive)")
-group.add_option("-c", "--quick-check", action="store_true", dest="quickCheck", default=False, help="Optionnal archive quick check mode")
-group.add_option("-C", "--thorough-check", action="store_true", dest="thoroughCheck", default=False, help="Optionnal archive thorough check mode")
-group.add_option("-d", "--next-start", dest="nextStart", help="Optionnal next start date")
-group.add_option("-f", "--file", action="append_const", dest="files", default=[], help="Specify file to archive")
+group = OptionGroup(parser, "restore archive options");
+group.add_option("-A", "--archive-id", dest="archiveId", type="int", help="Specify archive id")
+group.add_option("-d", "--next-start", dest="nextStart", default=None, help="Optionnal next start date")
+group.add_option("-f", "--file", action="append_const", dest="files", default=[], help="Specify file to restore, nothing for the whole archive")
 group.add_option("-j", "--job-name", dest="jobName", default=None, help="Optionnal job name")
-group.add_option("-m", action="append_const", dest="meta", default=[], help="Optionnal metadata")
-group.add_option("-p", "--pool-id", dest="poolId", type="int", help="Specify pool id (to create an archive)")
+group.add_option("-D", "--destination", dest="destination", default=None, help="Optionnal archive restoration destination")
 parser.add_option_group(group)
 
 group = OptionGroup(parser, "options for authenticate");
 group.add_option("-H", "--host", dest="host", default="localhost", help="Specify host name")
-group.add_option("-P", "--pwprompt", action="store_true", dest="promptPassword", default=False, help="If given, create-archive will issue a prompt for the password")
+group.add_option("-P", "--pwprompt", action="store_true", dest="promptPassword", default=False, help="If given, restore-archive will issue a prompt for the password")
 group.add_option("-U", "--username", dest="userName", default=None, help="Connect to api as the user username")
 group.add_option("-W", "--password", dest="password", help="Specify user password")
 parser.add_option_group(group)
@@ -32,17 +28,8 @@ group.add_option("-v", "--verbose", action="store_true", dest="verbose", default
 (options, args) = parser.parse_args()
 
 ok = True
-if options.archiveId is not None and options.archiveName is not None:
-    print("You should specify either an archive id or an archive name")
-    ok = False
-
-options.files.extend(args)
-if len(options.files) == 0:
-    print("You should specify file(s) to be archived")
-    ok = False
-
-if options.archiveId is not None and options.poolId is not None:
-    print("You should specify either an archive id or a pool id")
+if options.archiveId is None:
+    print("You should specify an archive id")
     ok = False
 
 if options.nextStart is not None:
@@ -60,35 +47,16 @@ if options.nextStart is not None:
         print("Failed to parse next start date parameter")
         ok = False
 
-if options.jobName is not None and options.archiveName is not None:
-    print("You should specify either an archive name or a job name")
-    ok = False
-
 if not ok:
     sys.exit(1)
 
 params = {
     'archive': options.archiveId,
-    'name': options.archiveName,
+    'name': options.jobName,
     'files': options.files,
-    'pool': options.poolId,
     'nextstart': options.nextStart,
-    'metadata': {},
-    'options': {}
+    'destination': options.destination
 }
-
-if options.jobName is not None:
-    params['name'] = options.jobName
-
-for meta in options.meta:
-    (key, value) = meta.split("=", 1)
-    params['metadata'][key] = value
-
-if options.thoroughCheck:
-    params['options']['thorough_mode'] = True
-
-if options.quickCheck:
-    params['options']['quick_mode'] = True
 
 if options.userName is None:
     print("User name: ", end="", flush=True)
@@ -113,26 +81,23 @@ if contentType is None or contentType != "application/json" or res.status != 201
 print ("Access granted")
 conn.close()
 
-# create archive
+# restore archive
 cookie = {'Cookie': res.getheader('Set-Cookie').split(';')[0]}
 headers.update(cookie)
 conn = http.client.HTTPConnection(options.host)
-if options.archiveName and options.poolId:
-    conn.request('POST', '/storiqone-backend/api/v1/archive/', json.dumps(params), headers)
-else:
-    conn.request('POST', '/storiqone-backend/api/v1/archive/add/', json.dumps(params), headers)
+conn.request('POST', '/storiqone-backend/api/v1/archive/restore/', json.dumps(params), headers)
 res = conn.getresponse()
 contentType = res.getheader('Content-type').split(';', 1)[0]
 if contentType is None or contentType != "application/json" or res.status != 201:
     message = res.read().decode("utf-8")
     conn.close()
     if options.verbose:
-        print ("Archival task creation has failed, response code : %d, params : %s, message: %s" % (res.status, params, message))
+        print ("Restoration task creation has failed, response code : %d, params : %s, message: %s" % (res.status, params, message))
     else:
-        print ("Archival task creation has failed")
+        print ("Restoration task creation has failed")
     sys.exit(3)
 
 message = json.loads(res.read().decode("utf-8"))
 conn.close()
 
-print("Archival task creation has succeeded, job id: %d" % (message['job_id']))
+print("Restoration task creation has succeeded, job id: %d" % (message['job_id']))
