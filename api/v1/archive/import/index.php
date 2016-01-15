8698011db0cd3193bcbfcafc4d5b96eef2ea1a28
hugo tagliero <htagliero@intellique.com>
2016-01-15 17:22:36 +0100

    Ajout de la fonction d'import d'une archive, avec la documentation et les jeux de test correspondants

---
+++ api/v1/archive/import/index.php
@@ -0,0 +1,206 @@
+<?php
+/**
+ * \addtogroup ArchiveImport Import an archive
+ * \section Import_archive_task Import archive task creation
+ * To create a Import archive task,
+ * use \b POST method
+ * \verbatim path : /storiqone-backend/api/v1/archive/import/ \endverbatim
+ * \param job : hash table
+ * \li \c archive id (integer) : archive id
+ * \li \c name [optional] (string) : import archive task name, <em>default value : "importArchive_" + archive name</em>
+ * \li \c nextstart [optional] (string) : import archive task nextstart date, <em>default value : now</em>
+ * \li \c options [optional] (hash table) : check archive options
+ * \param files : archive files array
+ * \li \c files (string array) : files to be added
+ * \return HTTP status codes :
+ *   - \b 201 Job created successfully
+ *     \verbatim New job id is returned
+ {
+    "message":"Job created successfully",
+    "job_id":6
+ }
+      \endverbatim
+ *   - \b 400 Bad request - Either ; archive id is required or archive id must be an integer or archive not found or incorrect input
+ *   - \b 401 Not logged in
+ *   - \b 403 Permission denied
+ *   - \b 500 Query failure
+ */
+	require_once("../../lib/env.php");
+
+	require_once("dateTime.php");
+	require_once("http.php");
+	require_once("session.php");
+	require_once("dbArchive.php");
+
+	switch ($_SERVER['REQUEST_METHOD']) {
+		case 'POST':
+			checkConnected();
+
+			if (!$_SESSION['user']['canarchive'])
+				httpResponse(403, array('message' => 'Permission denied'));
+
+			$formatInfo = httpParseInput();
+			// media id
+			if (!isset($formatInfo['media']))
+				httpResponse(400, array('message' => 'Media id is required'));
+
+			if (!is_int($formatInfo['media']))
+				httpResponse(400, array('message' => 'Media id must be an integer'));
+
+			// pool id
+			if (!isset($formatInfo['pool']))
+				httpResponse(400, array('message' => 'Pool id is required'));
+
+			if (!is_int($formatInfo['pool']))
+				httpResponse(400, array('message' => 'Pool id must be an integer'));
+
+			$dbDriver->startTransaction();
+
+			$ok = true;
+			$failed = false;
+
+			// check media
+			$media = $dbDriver->getMedia($formatInfo['media']);
+
+			if (!$media)
+				$dbDriver->cancelTransaction();
+
+			if ($media === null)
+				httpResponse(500, array('message' => 'Query failure'));
+			elseif ($media === false)
+				httpResponse(400, array('message' => 'Media not found'));
+
+			$pool = $dbDriver->getPool($formatInfo['pool']);
+
+			if (!$pool)
+				$dbDriver->cancelTransaction();
+
+			if ($pool === null)
+				httpResponse(500, array('message' => 'Query failure'));
+			elseif ($pool === false)
+
+				httpResponse(400, array('message' => 'Pool not found'));
+
+			if ($media['mediaformat']['id'] != $pool['mediaformat']['id']) {
+				$dbDriver->cancelTransaction();
+				httpResponse(400, array('message' => 'mediaformat of pool and media should match'));
+			}
+
+			if ($media['archiveformat']['id'] != $pool['archiveformat']['id']) {
+				$dbDriver->cancelTransaction();
+				httpResponse(400, array('message' => 'archiveformat of pool and media should match'));
+			}
+
+			if ($media['type'] == 'cleaning') {
+				$dbDriver->cancelTransaction();
+				httpResponse(400, array('message' => 'Cleaning media is not allowed to import archive'));
+			}
+
+			if ($media['status'] != 'foreign') {
+				$dbDriver->cancelTransaction();
+				httpResponse(400, array('message' => 'Selected media is not foreign'));
+			}
+
+			if ($media['pool'] != NULL) {
+				$dbDriver->cancelTransaction();
+				httpResponse(400, array('message' => 'Media sould not be a member of pool ("' . $pool['name'] . '")'));
+			}
+
+			if ($media['archiveformat'] == NULL) {
+				$dbDriver->cancelTransaction();
+				httpResponse(400, array('message' => 'Archive format should be known'));
+			}
+
+			if ($pool['deleted'] ) {
+				$dbDriver->cancelTransaction();
+				httpResponse(400, array('message' => 'Trying to import a media into a deleted pool'));
+			}
+
+			$job = array(
+				'interval' => null,
+				'backup' => null,
+				'media' => $formatInfo['media'],
+				'archive' => null,
+				'pool' => $formatInfo['pool'],
+				'login' => $_SESSION['user']['id'],
+				'metadata' => array(),
+				'options' => array()
+			);
+
+			// name [optional]
+			if (isset($formatInfo['name'])) {
+				$ok = is_string($formatInfo['name']);
+				if ($ok)
+					$job['name'] = $formatInfo['name'];
+			} elseif (isset($media['name']))
+				$job['name'] = "formatMedia_" . $media['name'];
+			 elseif (isset($media['label']))
+				$job['name'] = "formatMedia_" . $media['label'];
+			 elseif (isset($media['mediumserialnumber']))
+				$job['name'] = "formatMedia_" . $media['mediumserialnumber'];
+
+			// type
+			if ($ok) {
+				$jobType = $dbDriver->getJobTypeId("format-media");
+				if ($jobType === null || $jobType === false)
+					$failed = true;
+				else
+					$job['type'] = $jobType;
+			}
+
+			// nextstart [optional]
+			if ($ok && isset($infoJob['nextstart'])) {
+				$job['nextstart'] = dateTimeParse($infoJob['nextstart']);
+				if ($job['nextstart'] === null)
+					$ok = false;
+			} elseif ($ok)
+				$job['nextstart'] = new DateTime();
+
+
+			// options [optional]
+
+
+
+			// host
+			if ($ok) {
+				$host = $dbDriver->getHost(posix_uname()['nodename']);
+				if ($host === null || $host === false)
+					$failed = true;
+				else
+					$job['host'] = $host;
+			}
+
+			// gestion des erreurs
+			if ($failed || !$ok)
+				$dbDriver->cancelTransaction();
+
+			if ($failed)
+				httpResponse(500, array('message' => 'Query failure'));
+
+			if (!$ok)
+				httpResponse(400, array('message' => 'Incorrect input'));
+
+			$jobId = $dbDriver->createJob($job);
+
+			if ($jobId === null) {
+				$dbDriver->cancelTransaction();
+				httpResponse(500, array('message' => 'Query failure'));
+			}
+
+			$dbDriver->finishTransaction();
+
+			httpAddLocation('/job/?id=' . $jobId);
+			httpResponse(201, array(
+				'message' => 'Job created successfully',
+				'job_id' => $jobId,
+			));
+
+		case 'OPTIONS':
+			httpOptionsMethod(HTTP_POST);
+			break;
+
+		default:
+			httpUnsupportedMethod();
+			break;
+	}
+?>
