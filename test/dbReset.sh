2abaa2f6c20a171352884fdd1b4b1904d0c72bda
Zakary Nizard <znizard@intellique.com>
2017-05-17 13:04:19 +0200

    Mise à jour de la configuration de base

--- test/dbReset.sh
+++ test/dbReset.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#! /bin/bash
 # Database reset
 cd "$(dirname "$0")"
-psql -h taiko -f sql/psql/delete_database.sql storiqone-backend-vincent storiq
-psql -h taiko -f sql/psql/create_database.sql storiqone-backend-vincent storiq
-psql -h taiko -f sql/psql/insert_into_database.sql storiqone-backend-vincent storiq
+psql -h taiko -f sql/psql/delete_database.sql storiqone-backend-zakary storiq
+psql -h taiko -f sql/psql/create_database.sql storiqone-backend-zakary storiq
+psql -h taiko -f sql/psql/insert_into_database.sql storiqone-backend-zakary storiq
+# psql -h taiko -f sql/psql/storiqone.sql storiqone-backend-zakary storiq
+# pg_restore -d storiqone-backend-zakary -h taiko -U storiq sql/psql/storiqone.sql
