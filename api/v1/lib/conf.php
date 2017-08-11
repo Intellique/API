769d3d771c387a75c1eb23c585112f2b3a0ec60c
Clercin Guillaume <gclercin@intellique.com>
2017-08-11 10:05:50 +0200

    Relecture de library/index.php

--- api/v1/lib/conf.php
+++ api/v1/lib/conf.php
@@ -5,15 +5,16 @@
 	*/
 
 	$file = file_get_contents('/etc/storiq/storiqone.conf');
-	$config=json_decode($file, true);
+	$config = json_decode($file, true);
 
 	$db_config = array();
 
-	$db_config["driver"]   = $config['database'][0]['type'] ;
-	$db_config["host"]     = $config['database'][0]['host'] ;
-	$db_config["db"]       = $config['database'][0]['db'] ;
-	$db_config['user']     = $config['database'][0]['user'] ;
-	$db_config['password'] = $config['database'][0]['password'] ;
+	$db_config["driver"]   = $config['database'][0]['type'];
+	$db_config["host"]     = $config['database'][0]['host'];
+	$db_config["db"]       = $config['database'][0]['db'];
+	$db_config['user']     = $config['database'][0]['user'];
+	$db_config['password'] = $config['database'][0]['password'];
 	$db_config['port']     = isset($config['database'][0]['port']) ? $config['database'][0]['port'] : null;
 
 	unset($file);
+?>
--- api/v1/library/index.php
+++ api/v1/library/index.php
@@ -18,25 +18,24 @@
 * \section Library library ids (multiple list)
 * \verbatim path : /storiqone-backend/api/v1/library/ \endverbatim
 */
-    require_once("../lib/env.php");
+	require_once("../lib/env.php");
 
-    require_once("dateTime.php");
-    require_once("http.php");
-    require_once("session.php");
-    require_once("uuid.php");
-    require_once("dbLibrary.php");
+	require_once("dateTime.php");
+	require_once("http.php");
+	require_once("session.php");
+	require_once("uuid.php");
+	require_once("db.php");
 
+	switch ($_SERVER['REQUEST_METHOD']) {
+		case 'GET':
+			checkConnected();
 
-    switch ($_SERVER['REQUEST_METHOD']) {
-        case 'GET':
-            checkConnected();
+			if (!$_SESSION['user']['isadmin'] || !$_SESSION['user']['canarchive'] || !$_SESSION['user']['canrestore']) {
+				$dbDriver->writeLog(DB::DB_LOG_WARNING, sprintf('GET api/v1/library (%d) => Permission denied for a non-admin/archiver/restorer user', __LINE__), $_SESSION['user']['id']);
+				httpResponse(403, array('message' => 'Permission denied'));
+			}
 
-            if (!$_SESSION['user']['isadmin'] || !$_SESSION['user']['canarchive'] || !$_SESSION['user']['canrestore']) {
-                $dbDriver->writeLog(DB::DB_LOG_WARNING, 'GET api/v1/library => Permission denied for a non-admin/archiver/restorer user', $_SESSION['user']['id']);
-                httpResponse(403, array('message' => 'Permission denied'));
-            }
-
-            //REQ SQL permettant de lister les infos sur une librairie physique
+			//REQ SQL permettant de lister les infos sur une librairie physique
 			$physicalDevices = $dbDriver->getPhysicalLibraries();
 			if (!empty($physicalDevices)) {
 				foreach ($physicalDevices as &$physicalDevice) {
@@ -55,54 +54,54 @@
 				}
 			}
 
-            //REQ SQL permettant de lister les infos sur un lecteur autonome
-            $standaloneDevices = $dbDriver->getStandaloneDrives();
-            if (!empty($standaloneDevices))
-                foreach ($standaloneDevices as &$standaloneDevice) {
-                    $standaloneDevice['slot'] = $dbDriver->getSlotByDrive($standaloneDevice['driveid']);
+			//REQ SQL permettant de lister les infos sur un lecteur autonome
+			$standaloneDevices = $dbDriver->getStandaloneDrives();
+			if (!empty($standaloneDevices))
+				foreach ($standaloneDevices as &$standaloneDevice) {
+					$standaloneDevice['slot'] = $dbDriver->getSlotByDrive($standaloneDevice['driveid']);
 
 					if (isset($standaloneDevice['slot']['media']))
 						$standaloneDevice['slot']['media'] = $dbDriver->getMedia($standaloneDevice['slot']['media']);
 				}
 
-            //REQ SQL permettant de lister les infos sur une Virtual Tape Library
-            $VTLs = $dbDriver->getVTLs();
-            if (!empty($VTLs)) {
-                foreach ($VTLs as &$VTL) {
-                    $VTL['drives'] = $dbDriver->getDrivesByChanger($VTL['changerid']);
-                    foreach ($VTL['drives'] as &$drive)
-                        $drive['slot'] = $dbDriver->getSlotByDrive($drive['driveid']);
-                    $VTL['slots'] = $dbDriver->getSlotsByChanger($VTL['changerid']);
-                }
-            }
+			//REQ SQL permettant de lister les infos sur une Virtual Tape Library
+			$VTLs = $dbDriver->getVTLs();
+			if (!empty($VTLs)) {
+				foreach ($VTLs as &$VTL) {
+					$VTL['drives'] = $dbDriver->getDrivesByChanger($VTL['changerid']);
+					foreach ($VTL['drives'] as &$drive)
+						$drive['slot'] = $dbDriver->getSlotByDrive($drive['driveid']);
+					$VTL['slots'] = $dbDriver->getSlotsByChanger($VTL['changerid']);
+				}
+			}
 
-            $result_periphs = array(
-                'Tape Library' => array_values($physicalDevices),
-                'External Tape Drive' => array_values($standaloneDevices),
-                'Virtual Tape Library' => array_values($VTLs),
-            );
+			$result_periphs = array(
+				'Tape Library' => array_values($physicalDevices),
+				'External Tape Drive' => array_values($standaloneDevices),
+				'Virtual Tape Library' => array_values($VTLs),
+			);
 
-            if (count($physicalDevices) + count($standaloneDevices) + count($VTLs) == 0) {
-					httpResponse(404, array(
-						'message' => 'Device not found',
-                        'Tape Library' => NULL,
-                        'External Tape Drive' => NULL,
-                        'Virtual Tape Library' => NULL,
-					));
-            }
+			if (count($physicalDevices) + count($standaloneDevices) + count($VTLs) == 0) {
+				httpResponse(404, array(
+					'message' => 'Device not found',
+					'Tape Library' => NULL,
+					'External Tape Drive' => NULL,
+					'Virtual Tape Library' => NULL,
+				));
+			}
 
-            httpResponse(200, array(
-                    'message' => 'Query succeeded',
-                    'Tape Library' => array_values($physicalDevices),
-                    'External Tape Drive' => array_values($standaloneDevices),
-                    'Virtual Tape Library' => array_values($VTLs),
-            ));
+			httpResponse(200, array(
+				'message' => 'Query succeeded',
+				'Tape Library' => array_values($physicalDevices),
+				'External Tape Drive' => array_values($standaloneDevices),
+				'Virtual Tape Library' => array_values($VTLs),
+			));
 
 		case 'PUT':
 			checkConnected();
 
 			if (!$_SESSION['user']['isadmin'] || !$_SESSION['user']['canarchive'] || !$_SESSION['user']['canrestore']) {
-				$dbDriver->writeLog(DB::DB_LOG_WARNING, 'GET api/v1/library => Permission denied for a non-admin/archiver/restorer user', $_SESSION['user']['id']);
+				$dbDriver->writeLog(DB::DB_LOG_WARNING, sprintf('PUT api/v1/library (%d) => Permission denied for a non-admin/archiver/restorer user', __LINE__), $_SESSION['user']['id']);
 				httpResponse(403, array('message' => 'Permission denied'));
 			}
 
@@ -127,12 +126,13 @@
 				httpResponse(400, array('message' => 'library action is required'));
 			}
 
- 			$reponse = $dbDriver->setLibraryAction($id, $action);
+			$reponse = $dbDriver->setLibraryAction($id, $action);
 			$result = array('isUpdate' => $reponse);
 			httpResponse(200, array(
-                    'message' => 'Query succeeded',
-                    'return' => $reponse
-            ));
-			
-    }
+				'message' => 'Query succeeded',
+				'return' => $reponse
+			));
+
+			break;
+	}
 ?>
