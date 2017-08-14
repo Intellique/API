870d913f01eb565d6f2b5318416c374e8b699568
Clercin Guillaume <gclercin@intellique.com>
2017-08-14 11:40:53 +0200

    Relecture de pool/metadata/index.php

--- api/v1/pool/metadata/index.php
+++ api/v1/pool/metadata/index.php
@@ -24,57 +24,51 @@
 	require_once("http.php");
 	require_once("session.php");
 	require_once("dbArchive.php");
-	require_once("dbMetadata.php");
+	require_once("db.php");
 
 	switch ($_SERVER['REQUEST_METHOD']) {
-
 		case 'GET':
 			checkConnected();
 
-			$key = null;
 			if (isset($_GET['id'])) {
-				if (!is_numeric($_GET['id']))
+				if (filter_var($_GET['id'], FILTER_VALIDATE_INT) === false)
 					httpResponse(400, array('message' => 'id must be an integer'));
-
-				if (isset($_GET['key'])) {
-					if (!is_string($_GET['key']))
-						httpResponse(400, array('message' => 'key must be a string'));
-					$key = $_GET['key'];
-				}
 
 				$exists = $dbDriver->getPool($_GET['id']);
 				if ($exists === null) {
-					$dbDriver->writeLog(DB::DB_LOG_CRITICAL, 'GET api/v1/pool/metadata => Query failure', $_SESSION['user']['id']);
-					$dbDriver->writeLog(DB::DB_LOG_DEBUG, sprintf('getPool(%s)', $_GET['id']), $_SESSION['user']['id']);
+					$dbDriver->writeLog(DB::DB_LOG_CRITICAL, sprintf('GET api/v1/pool/metadata (%d) => Query failure', __LINE__), $_SESSION['user']['id']);
+					$dbDriver->writeLog(DB::DB_LOG_DEBUG, sprintf('GET api/v1/pool/metadata (%d) => getPool(%s)', __LINE__, $_GET['id']), $_SESSION['user']['id']);
 					httpResponse(500, array('message' => 'Query failure'));
-				}
-				if ($exists === false)
+				} elseif ($exists === false)
 					httpResponse(404, array('message' => 'This pool does not exist'));
 
-				$metadata = $dbDriver->getPoolMetadatas($_GET['id'], $key);
+				$metadata = $dbDriver->getPoolMetadatas($_GET['id'], $_GET['key']);
 				if ($metadata === null) {
-					$dbDriver->writeLog(DB::DB_LOG_CRITICAL, 'GET api/v1/pool/metadata => Query failure', $_SESSION['user']['id']);
-					$dbDriver->writeLog(DB::DB_LOG_DEBUG, sprintf('getPoolMetadatas(%s)', $_GET['id']), $_SESSION['user']['id']);
+					$dbDriver->writeLog(DB::DB_LOG_CRITICAL, sprintf('GET api/v1/pool/metadata (%d) => Query failure', __LINE__), $_SESSION['user']['id']);
+					$dbDriver->writeLog(DB::DB_LOG_DEBUG, sprintf('GET api/v1/pool/metadata (%d) => getPoolMetadatas(%s)', __LINE__, $_GET['id']), $_SESSION['user']['id']);
+
 					httpResponse(500, array(
 						'message' => 'Query failure',
 						'metadata' => array()
 					));
 				}
+
 				if ($metadata === false)
 					httpResponse(404, array(
-							'message' => 'No metadata found for this object',
-							'metadata' => array()
-						));
+						'message' => 'No metadata found for this object',
+						'metadata' => array()
+					));
+
 				if (count($metadata) === 0)
 					httpResponse(404, array(
-							'message' => 'No metadata found for this object',
-							'metadata' => $metadata
-						));
-
-				httpResponse(200, array(
-						'message' => 'Query succeeded',
+						'message' => 'No metadata found for this object',
 						'metadata' => $metadata
 					));
+
+				httpResponse(200, array(
+					'message' => 'Query succeeded',
+					'metadata' => $metadata
+				));
 			}
 			break;
 
