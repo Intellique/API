ac301eb489e961a730d79c12ac50690f70bbb931
Clercin Guillaume <gclercin@intellique.com>
2017-08-16 17:04:18 +0200

    Relecture de user/metadata/index.php

--- api/v1/lib/db/postgresqlMetadata.php
+++ api/v1/lib/db/postgresqlMetadata.php
@@ -40,6 +40,38 @@
 			return pg_affected_rows($result) > 0;
 		}
 
+		public function getJobMetadatas($id, $key) {
+			if (!isset($id) || !is_numeric($id))
+				return false;
+
+			if (!$this->prepareQuery('select_job_metadatas_by_id', "SELECT metadata FROM job WHERE id = $1"))
+				return null;
+
+			$result = pg_execute('select_job_metadatas_by_id', array($id));
+
+			if ($result === false)
+				return null;
+
+			if (pg_num_rows($result) == 0)
+				return false;
+
+			$meta = array();
+
+			while ($row = pg_fetch_array($result))
+				$meta[] = json_decode($row[0], true);
+
+			if (isset($key)) {
+				foreach($meta as $list) {
+					if (array_key_exists($key, $list))
+						return $list[$key];
+					else
+						return false;
+				}
+			}
+
+			return $meta;
+		}
+
 		public function getMetadata($id, $key, $type) {
 			if (!isset($id) || !isset($key) || !isset($type) || !is_numeric($id))
 				return array('error' => true, 'found' => false, 'value' => null);
@@ -134,38 +166,6 @@
 
 			while ($row = pg_fetch_array($result))
 				$meta[] = json_decode($row['meta'], true);
-
-			if (isset($key)) {
-				foreach($meta as $list) {
-					if (array_key_exists($key, $list))
-						return $list[$key];
-					else
-						return false;
-				}
-			}
-
-			return $meta;
-		}
-
-		public function getJobMetadatas($id, $key) {
-			if (!isset($id) || !is_numeric($id))
-				return false;
-
-			if (!$this->prepareQuery('select_job_metadatas_by_id', "SELECT metadata FROM job WHERE id = $1"))
-				return null;
-
-			$result = pg_execute('select_job_metadatas_by_id', array($id));
-
-			if ($result === false)
-				return null;
-
-			if (pg_num_rows($result) == 0)
-				return false;
-
-			$meta = array();
-
-			while ($row = pg_fetch_array($result))
-				$meta[] = json_decode($row[0], true);
 
 			if (isset($key)) {
 				foreach($meta as $list) {
