a521bcf8ecdde1698a0e7138c1caa4e7ce012cf5
Clercin Guillaume <gclercin@intellique.com>
2017-09-28 23:55:03 +0200

    Amélioration du preview des fichiers

--- api/v1/archivefile/preview/index.php
+++ api/v1/archivefile/preview/index.php
@@ -1,11 +1,13 @@
 <?php
 	require_once("../../lib/env.php");
 
+	require_once("conf.php");
 	require_once("session.php");
 	require_once("db.php");
 
 	switch ($_SERVER['REQUEST_METHOD']) {
 		case 'GET':
+		case 'HEAD':
 			checkConnected();
 
 			if (!isset($_GET['id']))
@@ -23,13 +25,33 @@
 			$archivefile = $dbDriver->getArchiveFile($_GET['id']);
 			if ($checkArchiveFilePermission === null)
 				httpResponse(500, array('message' => 'Query failure'));
-			elseif ($checkArchiveFilePermission === false || !posix_access($archivefile['name']))
+			elseif ($checkArchiveFilePermission === false)
 				httpResponse(404, array('message' => 'Not found'));
 
-			header('Content-Type: ' . $archivefile['mimetype']);
-			header('Content-Length: ' . $archivefile['size']);
+			if (isset($_GET['type'])) {
+				$mimetype = array("video/mp4" => ".mp4", "video/ogg" => ".ogv");
 
-			readfile($archivefile['name']);
+				if (!isset($mimetype[$_GET['type']]))
+					httpResponse(400, array('message' => 'type must be in "' . implode('", "', array_keys($mimetype)) . '"'));
+
+				$filename = $proxy_config['path'] . md5($archivefile['name']) . $mimetype[$_GET['type']];
+				if (!posix_access($filename))
+					httpResponse(404, array('message' => 'Not found'));
+
+				$file_info = stat($filename);
+
+				header('Content-Length: ' . $file_info['size']);
+			} elseif (posix_access($archivefile['name'])) {
+				$filename = $archivefile['name'];
+
+				header('Content-Type: ' . $archivefile['mimetype']);
+				header('Content-Length: ' . $archivefile['size']);
+			} else
+				httpResponse(404, array('message' => 'Not found'));
+
+
+			if ($_SERVER['REQUEST_METHOD'] == 'GET')
+				readfile($filename);
 
 			break;
 
